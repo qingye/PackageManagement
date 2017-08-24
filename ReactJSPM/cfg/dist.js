7cea56b06bb5670786e60c68ef767ca1c2d44488
qingye <qingye_love@qq.com>
2017-08-24 16:19:54 +0800

    【ReactJSPM】install 'html-webpack-plugin', auto inject split files into index.html

--- ReactJSPM/cfg/dist.js
+++ ReactJSPM/cfg/dist.js
@@ -7,10 +7,14 @@ let baseConfig = require('./base');
 let defaultSettings = require('./defaults');
 
 // Add needed plugins here
+let HtmlWebpackPlugin = require('html-webpack-plugin');
 let BowerWebpackPlugin = require('bower-webpack-plugin');
 
 let config = Object.assign({}, baseConfig, {
-  entry: path.join(__dirname, '../src/index'),
+  entry: {
+    bundle: path.join(__dirname, '../src/index'),
+    vendor: ['react', 'react-dom', 'react-router']
+  },
   cache: false,
   devtool: 'sourcemap',
   plugins: [
@@ -21,10 +25,23 @@ let config = Object.assign({}, baseConfig, {
     new BowerWebpackPlugin({
       searchResolveModulesDirectories: false
     }),
-    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'APP包管理',
+      filename: '../index.html',
+      template: 'src/index.html',
+      inject: 'head',
+      showErrors: true,
+      chunks: ['vendor']
+    })
   ],
   module: defaultSettings.getDefaultModules()
 });
